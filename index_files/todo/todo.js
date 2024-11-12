439c01823ca050a2a1ec6ee2e0ccb7fb8c48939d
Vlad Rotari <40292915+Siteograf@users.noreply.github.com>
2024-11-12 23:24:46 +0200

    Delete index_files directory

--- index_files/todo/todo.js
+++
@@ -1,9 +0,0 @@
-function toggleContent() {
-
-    const checkboxes = document.querySelectorAll('.toggleCheckbox');
-    const contentBlock = document.getElementById('contentBlock');
-    const contentTotal = document.getElementById('contentTotal');
-    const anyChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
-    contentBlock.style.display = anyChecked ? 'none' : 'block';
-    contentTotal.style.display = anyChecked ? 'block' : 'none';
-  }
